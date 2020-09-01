0a7c496f797e6e90df5838c554081aa051b69c3b
Javier Molina <impjim@Impripost.local>
2020-09-01 12:34:56 -0300

    update iterface icrud

--- gapharma_dal/Generic/ICRUD.cs
+++ gapharma_dal/Generic/ICRUD.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace gapharma_dal.Generic
 {
-    class ICRUD
+ 
+    public interface ICRUD<T>
     {
+        List<T> getAll();
+        T getByid(long id);
+
+        List<T> getByIdAll(long id);
+        void save(List<T> list);
+        void save(T obj);
+        void delete(T obj);
+        void update(T obj);
+        void update(List<T> lis);
+
     }
 }
